cf994684e0d1e5361b15dfa81dea1b694b74751c
Barış Akçay <embarisakcay@gmail.com>
2024-04-22 16:51:03 +0300

    Initial Commit

---
+++ Take Me Away/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Take Me Away
+//
+//  Created by Baris Akcay on 22.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
